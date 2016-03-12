e60d426eb28d6e88c318b1a3f51328263b39a984
leohowell <hongwang2193@gmail.com>
2016-03-12 20:21:54 +0800

    27. Remove Element accepted

---
+++ Easy/27.Remove_Element.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        try:
+            while True:
+                nums.remove(val)
+        except:
+            return len(nums)
