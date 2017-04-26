440440892e030f3bf03646e6e756f0f18ca69eec
leohowell <hongwang2193@gmail.com>
2017-04-26 16:22:49 +0800

    90. Subsets II accepted

---
+++ 90.Subsets_II.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return self.to_list(self.sub(nums)) + [[]]
+
+    def sub(self, nums):
+        if not nums:
+            return [{}]
+
+        if len(nums) == 1:
+            return [{nums[0]: 1}]
+
+        result = []
+        n = nums[0]
+        result.append({n: 1})
+        sub = self.sub(nums[1:])
+        for item in sub:
+            if not self.is_exists2(item, result):
+                result.append(item)
+
+            cand = {}
+            cand.update(item)
+            cand[n] = cand.get(n, 0) + 1
+            if not self.is_exists2(cand, result):
+                result.append(cand)
+        return result
+
+    def is_exists2(self, target, result):
+        for item in result:
+            if item == target:
+                return True
+        return False
+
+    def to_list(self, result):
+        r = []
+        for item in result:
+            t = []
+            for k, v in item.items():
+                for i in range(v):
+                    t.append(k)
+            r.append(t)
+        return r
+
+
+s = Solution()
+w = s.subsetsWithDup([1,2,2])
+print
+print len(w)
+r = [[],[1],[1,2],[1,2,2],[2],[2,2]]
+
+print sorted(w)
+print sorted(r)
+print len(r)
