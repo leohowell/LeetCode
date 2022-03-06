4513ef2e08f0c4761acb2d36b335ac7c0f17af20
leohowell <leohowell@leohowell.com>
2022-03-06 20:54:59 +0800

    q501 done

---
+++ 501.question.py
@@ -0,0 +1,105 @@
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return f"""<Node val={self.val} left={self.left} right={self.right}>"""
+
+
+def create_binary_tree_from_level_order_list(array):
+    """
+    从层次排序列表中生成二叉树
+    文档: https://support.leetcode-cn.com/hc/kb/article/1194353/
+    """
+    if not array:
+        return None
+
+    root = TreeNode(val=array[0])
+    last_nodes = [root]
+
+    pointer = 1
+    while pointer < len(array):
+        offset = len(last_nodes) * 2
+        # 当前值
+        current_level = array[pointer: pointer + offset]
+        pointer += offset
+        new_last_nodes = []
+        for index, node in enumerate(last_nodes):
+            left_index = index * 2
+            if left_index >= len(current_level):
+                break
+            left = current_level[index * 2]
+            if left:
+                left = TreeNode(val=left)
+                new_last_nodes.append(left)
+                node.left = left
+
+            right_index = index * 2 + 1
+            if right_index >= len(current_level):
+                break
+            right = current_level[right_index]
+            if right:
+                right = TreeNode(val=right)
+                new_last_nodes.append(right)
+                node.right = right
+        last_nodes = new_last_nodes
+
+    return root
+
+
+null = None
+
+
+from collections import defaultdict
+
+
+class Solution:
+    def __init__(self):
+        self.cnt = defaultdict(lambda: 0)
+
+    def findMode(self, root: TreeNode) -> List[int]:
+        self.dfs(root)
+        values = sorted(self.cnt.items(), key=lambda x:x[1])
+        max_cnt = values[-1][1]
+        res = []
+        for k, v in self.cnt.items():
+            if v == max_cnt:
+                res.append(k)
+        return res
+
+    def dfs(self, root):
+        if not root:
+            return
+        self.cnt[root.val] += 1
+        self.dfs(root.left)
+        self.dfs(root.right)
+
+
+
+def test():
+    func = lambda x: Solution().findMode(x)
+    root1 = create_binary_tree_from_level_order_list([5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, 1])
+    print(func(root1))
+    root2 = create_binary_tree_from_level_order_list([1,null,2,3])
+    print(func(root2))
+    root4 = create_binary_tree_from_level_order_list([1])
+    print(func(root4))
+    root5 = create_binary_tree_from_level_order_list([1, 2])
+    print(func(root5))
+    root6 = create_binary_tree_from_level_order_list([1,null,2])
+    print(func(root6))
+    root6 = create_binary_tree_from_level_order_list([3,9,20,null,null,15,7])
+    print(func(root6))
+    root6 = create_binary_tree_from_level_order_list([1,null,2,2])
+    print(func(root6))
+    root6 = create_binary_tree_from_level_order_list([0])
+    print(func(root6))
+
+
+if __name__ == '__main__':
+    test()
