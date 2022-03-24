2b1526f4439b4cb5005458beda7b958df780b402
leohowell <leohowell@leohowell.com>
2022-03-24 16:40:41 +0800

    sql 176 done

---
+++ 176.sql
@@ -0,0 +1,18 @@
+-- 176. 第二高的薪水
+-- https://leetcode-cn.com/problems/second-highest-salary/
+
+
+-- 我的解答
+select ifnull(
+               (select salary
+                from (select salary, rank() over (order by salary desc) salary_rank
+                      from Employee) a
+                where (salary_rank = 2) limit 1)
+, null
+           ) as SecondHighestSalary
+
+-- 官方题解
+select (select distinct salary
+        from Employee
+        order by salary
+           limit 1 offset 1) as SecondHighestSalary
