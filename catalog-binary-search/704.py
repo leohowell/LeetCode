16c61c0142c161bc31e9d7128d03bbdf53031f4a
leohowell <leohowell@leohowell.com>
2022-03-28 16:44:03 +0800

    catalog-binary-search 704 done

---
+++ catalog-binary-search/704.py
@@ -0,0 +1,29 @@
+# 704. 二分查找
+# https://leetcode-cn.com/problems/binary-search/
+
+
+# 我的解法
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums:
+            return -1
+
+        left = 0
+        right = len(nums)
+        middle = (right + left) // 2
+        while True:
+            if nums[middle] == target:
+                return middle
+            elif nums[middle] < target:
+                left = middle
+                middle = (right + left) // 2
+                if middle == left:
+                    return -1
+            else:
+                right = middle
+                middle = (right + left) // 2
+                if middle == right:
+                    return -1
