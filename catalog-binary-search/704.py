fe41c53cfa51167c45429d9eb33a0f3a916d2665
leohowell <leohowell@leohowell.com>
2022-03-28 17:47:52 +0800

    catalog-binary-search 704 update official answer

--- catalog-binary-search/704.py
+++ catalog-binary-search/704.py
@@ -27,3 +27,18 @@ class Solution:
                 middle = (right + left) // 2
                 if middle == right:
                     return -1
+
+
+# 官方题解
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums) - 1
+        while low <= high:
+            middle = (low + high) // 2
+            if nums[middle] == target:
+                return middle
+            elif nums[middle] > target:
+                high = middle - 1
+            else:
+                low = middle + 1
+        return -1
