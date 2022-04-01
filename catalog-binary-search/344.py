def4452ac5957c41384da88d48af5eb44d945357
leohowell <leohowell@leohowell.com>
2022-04-01 16:24:52 +0800

    catalog-two-pointers 283 my & official answer

---
+++ catalog-binary-search/344.py
@@ -0,0 +1,12 @@
+# 344. 反转字符串
+# https://leetcode-cn.com/problems/reverse-string/
+
+# 我的解法
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        length = len(s)
+        for i in range(len(s) // 2):
+            s[i], s[len(s) - i - 1] = s[len(s) - i - 1], s[i]
