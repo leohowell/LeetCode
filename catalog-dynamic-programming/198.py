62c6b2a5bd6fa0f4468d3534583b7ceaf8809c4a
leohowell <leohowell@leohowell.com>
2022-04-21 16:18:27 +0800

    catalog-dynamic-programming 198 add dp version solution

--- catalog-dynamic-programming/198.py
+++ catalog-dynamic-programming/198.py
@@ -24,3 +24,21 @@ class Solution(object):
 
 print(Solution().rob([6, 11, 2, 17, 16, 0, 9, 5, 9, 3]))
 print(Solution().rob([100, 100000, 10, 1000]))
+
+
+# 动态规划版本
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+
+        # dp[] 代表当前房间数能够偷窃到的最大金额
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, n):
+            # 状态转移方程
+            dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+        return dp[-1]
