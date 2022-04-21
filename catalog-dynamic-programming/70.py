03c5992e2289540dd210f7c94aeeeae4bfff85aa
leohowell <leohowell@leohowell.com>
2022-04-21 16:04:09 +0800

    catalog-dynamic-programming 70 my answer

--- catalog-dynamic-programming/70.py
+++ catalog-dynamic-programming/70.py
@@ -13,3 +13,13 @@ class Solution:
         res = self.climbStairs(n-1) + self.climbStairs(n-2)
         cache[n] = res
         return res
+
+
+def solution(n):
+    # dp为到第n阶所需的最小次数
+    dp = [0] * n + 1
+    dp[0] = 1
+    dp[1] = 2
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[n]
